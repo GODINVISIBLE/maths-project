93cc8c7a2e4d416af330f7e462212bbe35382700
MR INVISIBLE <114129087+GODINVISIBLE@users.noreply.github.com>
2022-09-24 00:39:02 -0700

    Add files via upload

---
+++ first program.py
@@ -0,0 +1,35 @@
+#Maths Project 
+
+numb = int(input("Eneter the Number : "))
+numb2 = int(input("Eneter The number : "))
+
+answer = numb+numb2
+
+print(answer)
+
+a = int(input("Eneter Your number "))
+b = int(input("Enter Your Number  "))
+
+ans = a-b
+
+print(ans)
+
+numb_1 = int(input("Enter Your Nnumb "))
+numb_2 = int(input("Enter Your NUmber "))
+
+ans_1 = numb_1*numb_2
+
+print(ans_1)
+
+numb_01 = int(input("Eneter Your Numb "))
+numb_02 = int(input("Eneter Your Numb "))
+
+ans_2 = numb_01/numb_02
+
+print(ans_2)
+
+i = int(input("Eneter Your Numb "))
+s = int(input("Eneter Your Numb "))
+
+res = i%s
+print(res)
